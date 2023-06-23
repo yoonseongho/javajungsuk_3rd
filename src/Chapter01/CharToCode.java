2545cb98b2241aecfa1c62d677268721642ada68
yunseongho <sxdz1290@naver.com>
2023-06-23 23:01:24 +0900

    init

--- src/Chapter01/CharToCode.java
+++ src/Chapter01/CharToCode.java
@@ -1,2 +1,13 @@
-package Chapter01;public class CharToCode {
+package Chapter01;
+
+public class CharToCode {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("%c=%d(%#X)%n", ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+    }
 }
--- src/Chapter01/ScannerEx.java
+++ src/Chapter01/ScannerEx.java
@@ -1,2 +1,16 @@
-package Chapter01;public class ScannerEx {
+package Chapter01;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("두자리 정수를 하나 입력해주세요.>");
+        String input = sc.nextLine();
+        int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
+
+        System.out.println("입력 내용 :" + input);
+        System.out.printf("num=%d%n", num);
+    }
 }
