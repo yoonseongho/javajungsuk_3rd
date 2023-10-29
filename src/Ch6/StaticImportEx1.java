97210295ca822fb7c689e67ac7aa7c81a1ea2437
SeeQ <sxdz1290@naver.com>
2023-10-30 00:16:52 +0900

    20231030

---
+++ src/Ch6/StaticImportEx1.java
@@ -0,0 +1,12 @@
+package Ch6;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportEx1 {
+    public static void main(String[] args) {
+
+        out.println(random());
+        out.println("Math.PI :" + PI);
+    }
+}
---
+++ src/Ch6/SuperTest.java
@@ -0,0 +1,20 @@
+package Ch6;
+
+class SuperTest {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.method();
+    }
+}
+
+class Parent {
+    int x=10;
+}
+
+class Child extends Parent {
+    void method() {
+        System.out.println("x="+x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super=" + super.x);
+    }
+}
